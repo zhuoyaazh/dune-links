d2eeb5fc6e1f3491514a3cf320bb7dd171ae6de0
zhuoyaazh <najtnsyf12@gmail.com>
2026-01-03 20:03:33 +0700

    fix: remove webpack config and worker-loader, use Next.js native web worker support

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.worker\.ts$/,
-      use: { loader: "worker-loader" },
-    });
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
